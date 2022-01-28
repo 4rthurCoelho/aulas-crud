1bf0e7a05b5b39b7d68dc593c9a3a9ae92be43b1
Arthur <arthur.coelhojb@gmail.com>
2022-01-28 02:36:08 -0300

    Parte dinamica feita de acordo com as aulas 48 e 49 co curso de php

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
+//conexão
+include_once 'php_action/db_connect.php';
+
 include_once 'includes/header.php';
+
+include_once 'includes/mensagem.php';
 ?>
 
 <div class="row">
@@ -16,18 +21,24 @@ include_once 'includes/header.php';
             </thead>
 
             <tbody>
+                <?php
+                $sql = "SELECT * FROM clientes";
+                $resultado = mysqli_query($connect, $sql);
+                while($dados = mysqli_fetch_array($resultado)):
+                ?>
                 <tr>
-                    <td>Matheus</td>
-                    <td>Oliveira</td>
-                    <td>matheusluisantoniooliveira-97@emayl.com</td>
-                    <td>20</td>
+                    <td><?php echo $dados['nome']; ?></td>
+                    <td><?php echo $dados['sobrenome']; ?></td>
+                    <td><?php echo $dados['email']; ?>-97@emayl.com</td>
+                    <td><?php echo $dados['idade']; ?></td>
                     <td><a href="" class="btn-floating purple"><i class="material-icons">edit</i></a></td>
                     <td><a href="" class="btn-floating pink"><i class="material-icons">delete</i></a></td>
                 </tr>
+                <?php endwhile; ?>
             </tbody>
         </table>
         <br>
-        <a href="" class="btn purple">Adicionar cliente</a>
+        <a href="adicionar.php" class="btn purple">Adicionar cliente</a>
     </div>
 </div>
 
